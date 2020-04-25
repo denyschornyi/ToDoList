c9982281bff49eefeac2495a315246d4e52e2f01
Denys <chornijj14@gmail.com>
2020-04-26 00:34:47 +0300

    Start from begin. Create appended element

--- js/script.js
+++ js/script.js
@@ -1,33 +1,46 @@
-let taskInput = document.querySelector('.task-input'),
-    ulToDo    = document.querySelector('.todo');
+//Selectors
+let todoInput = document.querySelector('.todo-input'),
+    todoList   = document.querySelector('.todo-list');
 
-let toDoList = [];
+let todoArray = [];
+//Event Listener
+todoInput.addEventListener('keyup', addTodo);
 
-taskInput.addEventListener('keyup', (event)=>{
-    if(!taskInput.value) return;
-    if(event.keyCode === 13){
-        let newToDo = {
-            item: taskInput.value,
+//Functions
+
+function addTodo(event){
+    if(event.keyCode === 13){   
+       
+        let newTodo = {
+            item: todoInput.value,
             checked: false
         }
-        toDoList.push(newToDo);
-        displayMessages();
-        taskInput.value = '';
-    }
-});
 
-function displayMessages(){
-    let displayMessage = '';
-    toDoList.forEach(function(item, i){
-        displayMessage += `
-            <li>
-                <input type='checkbox' id='item-${i}'>
-                <label for='item-${i}'> ${item.item}</label>
-                <button class='clear' >&#10005;</button>
-            </li>
-        `;
-        ulToDo.innerHTML = displayMessage;
-    });
+        //CREATE ELEMENT TODO
+        //CREATE ELEMENT LI  WRAPPER
+        const taskLi = document.createElement('LI');
+        taskLi.classList.add('task-li');
+        //CREATE CHECKBOX
+        const taskCheckbox = document.createElement('INPUT');
+        taskCheckbox.type = 'checkbox';
+        taskLi.appendChild(taskCheckbox);
+        //CREATE INPUT TASK LABEL
+        const taskLabel = document.createElement('input');
+        taskLabel.type = 'text';
+        taskLabel.classList.add('task-label');
+        taskLabel.value = newTodo.item;
+        taskLabel.setAttribute("disabled", "disabled");
+        taskLi.appendChild(taskLabel);
+        //CREATE DELETE BUTTON
+        const deleteBtn = document.createElement('BUTTON');
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.innerHTML = '&times;';
+        taskLi.appendChild(deleteBtn);
+
+        //APPEND ELEMENT TO DOM ELEMENT TODO LIST
+        todoList.appendChild(taskLi);
+        todoArray.push(newTodo);
+        todoInput.value = '';
+        console.log(todoArray);
+   }
 }
-
-
