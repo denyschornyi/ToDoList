6b63f9ff08ed8763e527f99fd37909b05a24b0e7
Denys <chornijj14@gmail.com>
2020-04-26 13:01:18 +0300

    seperate to diferent function

--- js/script.js
+++ js/script.js
@@ -4,40 +4,39 @@ let todoInput = document.querySelector('.todo-input'),
 
 let todoArray = [];
 //Event Listener
-todoInput.addEventListener('keyup', addTodo);
+todoInput.addEventListener('keyup', check);
 todoList.addEventListener('click', removeItem);
 
 //Functions
-
-function addTodo(event){
+function check(event){
+    if(!todoInput.value) return;
     if(event.keyCode === 13){   
         let newTodo = {
             item: todoInput.value,
             checked: false,
         }
         todoArray.push(newTodo);
-
-        let displayMessage = '';
-        todoArray.forEach(function(item, i){
-            displayMessage += `
-            <li class="task-li">
-                <input type="checkbox" class="check" id="item-${i}" >
-                <label for="item-${i}" class="task-label">${item.item}</label>
-                <button class="delete-btn">&times;</button>
-            </li>
-        `;
-        });
-        //APPEND ELEMENT TO DOM ELEMENT TODO LIST
-        todoList.innerHTML = displayMessage;
-        
+        addTodo();
         todoInput.value = '';
         console.log(todoArray);
-   }
+    }
+}
+
+function addTodo(event){
+    let displayMessage = '';
+    todoArray.forEach(function(item, i){
+    displayMessage += `
+        <li class="task-li">
+            <input type="checkbox" class="check" id="item-${i}" >
+            <label for="item-${i}" class="task-label">${item.item}</label>
+            <button class="delete-btn">&times;</button>
+        </li>`;
+    todoList.innerHTML = displayMessage; //APPEND ELEMENT TO DOM ELEMENT TODO LIST
+    });
 }
 
 function removeItem(event){
     let item = event.target;
-
     if(item.classList.contains('delete-btn')){
         console.log(item.parentElement);
         item.parentElement.remove();
